34aedf2e6ace34c14ed2f0119c0acf6b52da34dd
lorenanfante <linfante@modyo.com>
2021-12-17 15:25:27 -0300

    Ejercicio 2 y lecciones 22-26
    Ejercicio 3 (lección 27) en proceso

--- udemy-Vue/basics-03-events-starting-code/app.js
+++ udemy-Vue/basics-03-events-starting-code/app.js
@@ -2,8 +2,32 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:'',
+      confirmedName:''
     };
   },
+  methods: {
+    
+    submitForm(){
+      alert('Request sent!');
+      
+    },
+    add(num){
+      this.counter = this.counter + num;
+    },
+
+    reduce(num){
+      this.counter = this.counter  - num;
+    },
+
+    setName(e, lastName){
+      this.name = e.target.value + ' ' +lastName;
+
+    },
+    confirmInput(){
+      this.confirmedName = this.name;
+    }
+  }
 });
 
 app.mount('#events');
---
+++ udemy-Vue/basics-assignment-2-problem/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp ({
+    
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+        showAlert(){
+            alert('Hey! An alert here!');
+        }
+
+    }
+
+
+});
+
+app.mount("#assignment");
