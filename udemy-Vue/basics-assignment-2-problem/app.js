df8922e831e45261e629597bca5f8e9474b54754
lorenanfante <linfante@modyo.com>
2021-12-21 11:16:16 -0300

    ejercicio 2 solved

--- udemy-Vue/basics-assignment-2-problem/app.js
+++ udemy-Vue/basics-assignment-2-problem/app.js
@@ -2,12 +2,20 @@ const app = Vue.createApp ({
     
     data(){
         return{
+            inputText:'',
+            inputTextEnter:''
             
         }
     },
     methods:{
         showAlert(){
             alert('Hey! An alert here!');
+        },
+        showInput(e){
+            this.inputText = e.target.value;
+        },
+        showInputEnter(e){
+            this.inputTextEnter = e.target.value;
         }
 
     }
