939a6f55dbe987aa4f7390169ac43fd0af477e69
lorenanfante <linfante@modyo.com>
2022-01-23 18:42:12 -0300

    computed properties

--- udemy-Vue/basics-03-events-starting-code/app.js
+++ udemy-Vue/basics-03-events-starting-code/app.js
@@ -6,8 +6,20 @@ const app = Vue.createApp({
       confirmedName:''
     };
   },
+  computed:{
+//the computed properties are methods that don't need to be called with () inside html. This function works as a function but is rendered as an object of data() and it's executed the times that the variable used is modified. Do not affect the whole app.
+    fullName(){
+      
+      if(this.name === ''){
+        return this.name ='';
+      }
+      console.log('It is running again'); 
+      return this.name + ' ' + 'Infante';
+    }
+
+  },
   methods: {
-    
+    //methods are still called inside our v-on, v.click, etc
     submitForm(){
       alert('Request sent!');
       
@@ -30,7 +42,8 @@ const app = Vue.createApp({
     resetInput(){
       this.name = '';
     },
-    //If outputFullName is called as a method inside html it will affect the performance
+    /*If outputFullName is called as a method inside html it will affect the performance.
+     Se recarga TODA la página*/
     outputFullName(){
       console.log('Running Again');
       if(this.name === ''){
