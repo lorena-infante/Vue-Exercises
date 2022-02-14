972baaf02111eefb263415ab791e2ec6d491fc30
lorenanfante <linfante@modyo.com>
2022-02-14 18:49:23 -0300

    centro de ayuda slider- widget 14 feb

---
+++ udemy-Vue/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,34 @@
+//If you DO NOT need access to Vue controlled data properties, if you're NOT calling the function from the inside HTML template, and you only USE IT FROM JS code, we can create just a JS normal function
+
+function getRandomValue(min, max) {
+	//calcultes random value of damage between a min and max value
+	Math.floor(Math.random() * (max - min)) + min;
+	console.log(Math.floor(Math.random() * (max - min)) + min);
+}
+
+const app = Vue.createApp({
+	data() {
+		return {
+			playerHealth: 100,
+			monsterHealth: 100,
+		};
+	},
+
+	methods: {
+		//reduces monster's health
+		attackMonster() {
+			//Math.floor(Math.random()* (max-min))+ min;
+			const attackValue = getRandomValue(5, 12);
+			//reduces monter's health this.monsterHealth = this.monsterHealth - attackValue;
+			this.monsterHealth -= attackValue;
+
+			//we can access methods through "this"
+			this.attackPlayer();
+		},
+		attackPlayer() {
+			const attackValue = getRandomValue(8, 15);
+			this.playerHealth -= attackValue;
+		},
+	},
+});
+app.mount("#game");
