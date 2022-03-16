a77002df85b4607ba1d0525dac4c3ddf2c19eabc
lorenanfante <linfante@modyo.com>
2022-03-16 15:40:42 -0300

    está raro el health del monster cuando llega después de 0

--- udemy-Vue/prj-monster-01-starting-setup/app.js
+++ udemy-Vue/prj-monster-01-starting-setup/app.js
@@ -19,22 +19,59 @@ const app = Vue.createApp({
 			this.currentRound++;
 			//Math.floor(Math.random()* (max-min))+ min;
 			const attackValue = getRandomValue(5, 12);
-			//reduces monster's health this.monsterHealth = this.monsterHealth - attackValue;
-			this.monsterHealth -= attackValue;
+
+			if (this.monsterHealth <= 0) {
+				this.monsterHealth = 0;
+			} else {
+				//reduces monster's health this.monsterHealth = this.monsterHealth - attackValue;
+				this.monsterHealth -= attackValue;
+			}
 
 			//we can access methods through "this"
 			this.attackPlayer();
 		},
 		attackPlayer() {
 			const attackValue = getRandomValue(8, 15);
-			this.playerHealth -= attackValue;
+			if (this.playerHealth <= 0) {
+				this.playerHealth = 0;
+			} else if (this.playerHealth - attackValue < 0) {
+				this.playerHealth = 0;
+			} else {
+				this.playerHealth -= attackValue;
+			}
 		},
 
 		specialAttackMonster() {
 			this.currentRound++;
 			const attackValue = getRandomValue(10, 25);
-			this.monsterHealth -= attackValue;
+			if (this.monsterHealth <= 0) {
+				this.monsterHealth = 0;
+			} else if (this.monsterHealth - attackValue < 0) {
+				this.monsterHealth = 0;
+			} else {
+				this.monsterHealth -= attackValue;
+			}
+
 			this.attackPlayer();
+		},
+		healPlayer() {
+			this.currentRound++;
+			const healthValue = getRandomValue(8, 20);
+			if (this.playerHealth + healthValue > 100) {
+				this.playerHealth = 100;
+			} else {
+				this.playerHealth += healthValue;
+			}
+			this.attackPlayer();
+		},
+		getMatchResults() {
+			if (this.playerHealth === 0) {
+				alert("You've Lost!");
+			} else if (this.monsterHealth === 0) {
+				alert("Monster's Lost!");
+			} else if (this.monsterHealth === 0 && this.playerHealth === 0) {
+				alert("This is a Draw");
+			}
 		},
 	},
 	computed: {
@@ -49,6 +86,9 @@ const app = Vue.createApp({
 		shouldUseSpecialAttack() {
 			/*It should be executed only passed 3 rounds*/
 			return this.currentRound % 3 !== 0;
+		},
+		getResults() {
+			return this.playerHealth === 0 || this.monsterHealth === 0;
 		},
 	},
 });
