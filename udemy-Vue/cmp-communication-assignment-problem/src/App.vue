30845d66c65804cdb030335b50b432ada36e2163
lorenanfante <linfante@modyo.com>
2022-05-26 10:01:50 -0400

    feat: activeUsers and UserData components, main configured

---
+++ udemy-Vue/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <user-data />
+  </div>
+</template>
+
+<script>
+import UserData from './components/UserData.vue';
+
+
+export default {
+  name:'App',
+  components:{
+    UserData,
+  },
+}
+</script>
---
+++ udemy-Vue/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="user-data">
+    <form class="user-data-form">
+      <label for="name">Name</label>
+      <input type="text" placeholder="add your name here">
+      <label for="name">Age</label>
+      <input type="text" placeholder="add your age here">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+
+  };
+},
+}
+</script>
+<style scoped lang="css">
+  .user-data-form {
+    display:flex;
+  }
+
+</style>
