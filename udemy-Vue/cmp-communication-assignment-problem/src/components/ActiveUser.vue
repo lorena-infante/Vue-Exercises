823d154ca660ca27b2a602d8f308046f7ecc4666
lorenanfante <linfante@modyo.com>
2022-05-31 10:19:32 -0400

    feat: styling new comp

---
+++ udemy-Vue/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="active-user">
+    <div class="name">
+      <h1>Name:</h1>
+      <user-data>
+    </div>
+    <div class="age">
+      <h1>Age</h1>
+      <user-data>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
