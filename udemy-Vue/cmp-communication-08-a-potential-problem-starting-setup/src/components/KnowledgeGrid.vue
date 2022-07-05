5e57df947e31f2cb7aea4bb9dc025a027942b49f
lorenanfante <linfante@modyo.com>
2022-07-05 17:24:37 -0400

    feat: provide and inject

---
+++ udemy-Vue/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  //inject works like props BUT only with components in a HIGHER UP level (parents)
+  inject: ['topics'],
+  emits: ['select-topic']
+};
+</script>
