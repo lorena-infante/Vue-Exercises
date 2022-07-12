7d10a77c66362f1296e5b8d11e0273d9d2532a80
lorenanfante <linfante@modyo.com>
2022-07-12 16:49:56 -0400

    feat: scoped slots

--- udemy-Vue/cmp-adv-01-starting-setup/src/App.vue
+++ udemy-Vue/cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <!-- slotProps is the name of the object that will get the props of the slot (it could be any name). -->
+      <!-- <template> if the template that we are using is the default one for the slot, we can remove the <template> tag and add the #default to the component that is using that slot -->
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.another }}</p>
+      <!-- </template> -->
+    </course-goals>
   </div>
 </template>
 
@@ -14,12 +21,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals,
 
 
   },
---
+++ udemy-Vue/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <!-- scoped slots are used to format certain data, using props from the component to the App.vue -->
+      <slot :item="goal" another="ola" ></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      goals: ['Finish the course','Learn Vue'],
+    };
+  }
+}
+</script>
