c115ccc8ab6b4256ca4802cf27b5aeb751b54d5f
lorenanfante <linfante@modyo.com>
2022-07-13 14:52:00 -0400

    feat: teleporting and linter config

--- udemy-Vue/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ udemy-Vue/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -3,11 +3,14 @@
   <h2>Manage Goals</h2>
   <input type="text" ref="goal">
   <button @click="setGoal()">Set goal </button>
-  <error-alert v-if="isInputInvalid">
-    <h2>Please enter a valid input</h2>
-    <p>At least 1 char, don't be lazy</p>
-    <button class="error" @click="setClose()">Close and start again</button>
-  </error-alert>
+  <!--teleport helps us to move the content to another place inside the DOM-->
+  <teleport to="body">
+    <error-alert v-if="isInputInvalid">
+      <h2>Please enter a valid input</h2>
+      <p>At least 1 char, don't be lazy</p>
+      <button class="error" @click="setClose()">Close and start again</button>
+    </error-alert>
+  </teleport>
 </div>
 
 </template>
