2c458f52d329f74be5e272a0f96d0c506cf27cd1
lorenanfante <linfante@modyo.com>
2022-07-14 15:12:05 -0400

    feat: adding appvue and mainjs

---
+++ udemy-Vue/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<h1>Lista de vainas</h1>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      storedResources:[
+        { id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue documentation',
+          link: 'https://vuejs.org'
+        },
+        { id: 'google',
+          title: 'Google',
+          description: 'Learn how to google',
+          link: 'https://google.com'
+        },
+        { id: 'udemy',
+          title: 'Udemy',
+          description: 'Learn everything',
+          link: 'https://udemy.com'
+        },
+      ],
+    }
+  },
+  
+}
+</script>
