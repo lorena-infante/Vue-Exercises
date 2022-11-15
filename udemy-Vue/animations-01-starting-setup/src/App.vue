f58b47ce7133656e7e50be6a856b1a424ffcad55
lorenanfante <linfante@modyo.com>
2022-11-15 10:01:33 -0300

    feat: animating a modal

--- udemy-Vue/animations-01-starting-setup/src/App.vue
+++ udemy-Vue/animations-01-starting-setup/src/App.vue
@@ -11,10 +11,10 @@
     </transition>
     <button @click="toggleParagraph">Toggle paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
