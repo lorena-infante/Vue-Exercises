1c3f7f28432628ac9ccda4ff44437ae469def439
lorenanfante <linfante@modyo.com>
2023-03-13 10:12:20 -0300

    feat: 205. transition group

--- udemy-Vue/animations-01-starting-setup/src/components/UsersList.vue
+++ udemy-Vue/animations-01-starting-setup/src/components/UsersList.vue
@@ -1,11 +1,11 @@
 <template>
-  <ul>
-    <li v-for="user in users" 
-      @click="removeUser(user)" 
-      :key="user">
-      {{ user }}
-    </li>
-  </ul>
+    <transition-group tag="ul" name="user-list">
+      <li v-for="user in users" 
+        @click="removeUser(user)" 
+        :key="user">
+        {{ user }}
+      </li>
+    </transition-group>
   <div>
     <input type="text" ref="userNameInput"/>
     <button @click="addUser()">Add User</button>
@@ -43,6 +43,29 @@ export default {
     border: 1px solid #ccc;
     padding: 1rem;
     text-align: center;
+  }
 
+  .user-list-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+  }
+  .user-list-enter-active {
+    transition: all 1s ease;
+  }
+  .user-list-enter-to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+
+  .user-list-leave-from {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+  .user-list-leave-active {
+    transition: all 1s ease-out;
+  }
+  .user-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
   }
 </style>
