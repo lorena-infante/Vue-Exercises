01d26dc1d228599bca7fa1c9cddbc28e806ddd0a
lorenanfante <linfante@modyo.com>
2023-03-14 18:13:27 -0300

    feat 216. getters

--- udemy-Vue/vuex-01-starting-setup/src/App.vue
+++ udemy-Vue/vuex-01-starting-setup/src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
-              <button @click="addOne">Add 10</button>
-                <change-counter></change-counter>
-            </base-container>
+      <favorite-value></favorite-value>
+      <button @click="addOne">Add 10</button>
+      <change-counter></change-counter>
+    </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
     ChangeCounter,
+    FavoriteValue
   },
 
   methods: {
--- udemy-Vue/vuex-01-starting-setup/src/main.js
+++ udemy-Vue/vuex-01-starting-setup/src/main.js
@@ -5,19 +5,35 @@ import { createStore} from 'vuex';
 import App from './App.vue';
 
 const store = createStore({
-  state(){
-    return {
-      counter: 0,
-    };
+state(){
+  return {
+    counter: 0,
+  };
+},
+mutations: {
+  increment(state){
+    state.counter = state.counter + 2;
   },
-  mutations: {
-    increment(state){
-      state.counter = state.counter + 2;
-    },
-    increase(state, payload){
-      state.counter = state.counter + payload.value;
-    }
+  increase(state, payload){
+    state.counter = state.counter + payload.value;
   }
+},
+
+getters: {
+  finalCounter(state){
+    return state.counter * 3;
+  },
+  normalizedCounter(_, getters){
+    const finalCounter = getters.finalCounter;
+    if (finalCounter < 0 ){
+      return 0;
+    }
+    if (finalCounter > 100){
+      return 100;
+    }
+    return finalCounter;
+  }
+}
 });
 
 const app = createApp(App);
