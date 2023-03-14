6c1d1607a57c7941520bc753e121ed1648dba8dc
lorenanfante <linfante@modyo.com>
2023-03-14 17:01:08 -0300

    feat: 214. mutations

---
+++ udemy-Vue/vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="addOne"> Add 1</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    addOne() {
+      //this is the name of the method inside the mutation that I want to call
+      this.$store.commit('increment');
+    }
+  }
+}
+</script>
